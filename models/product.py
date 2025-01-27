6a7bf8027dca10def62fcf4fa0249f4f836c2664
Gaston <gastoncorrea90e@gmail.com>
2025-01-27 14:30:28 -0300

    create productCLass

---
+++ models/product.py
@@ -0,0 +1,8 @@
+from app import db, ForeignKey
+
+class Product(db.Model):
+
+    __tablename__ = 'producto'
+
+    id_producto = db.column(db.Integer, primary_key=True)
+    nombre_producto = db.Column(db.String(100), nullable=False)

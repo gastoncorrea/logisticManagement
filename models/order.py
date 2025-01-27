5defaa49a1e153c74e8c0d2690410342a1f3d583
Gaston <gastoncorrea90e@gmail.com>
2025-01-27 14:27:30 -0300

    create orderDetail Class and Model

--- models/order.py
+++ models/order.py
@@ -4,7 +4,7 @@ class Order(db.Model):
     __tablename__ = 'pedido'
 
     id_pedido = db.Column(db.Integer, primary_key = True)
-    nro_pedido = db.Column(db.String(45), nullable = False) 
+    nro_pedido = db.Column(db.String(45), nullable = False, unique = True) 
     fecha = db.Column(db.Date(), nullable = False) 
     id_cliente = db.Column(db.Integer(), ForeignKey("Cliente_id_cliente"))
     Ubicacion_id_ubicacion = db.Column(db.Integer(), ForeignKey("Ubicacion_id_cliente"))
---
+++ models/orderDetail.py
@@ -0,0 +1,10 @@
+from app import db, ForeignKey
+
+class OrderDetail(db.Model):
+
+    __tablename__ = 'detalle_pedido'
+
+    id_detalle_pedido = db.column(db.Integer, primary_key=True)
+    cantidad = db.Column(db.Integer, nullable=False)
+    id_producto = db.Column(db.Integer, ForeignKey("Producto_id_producto"))
+    id_pedido = db.Column(db.Integer, ForeignKey("Pedido_id_pedido"))

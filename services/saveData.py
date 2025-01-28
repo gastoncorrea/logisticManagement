6073a8a2359a19b248b64d0be04d6d092d8b333a
Gaston <gastoncorrea90e@gmail.com>
2025-01-27 23:12:52 -0300

    add autoincrement=True in all models

--- services/saveData.py
+++ services/saveData.py
@@ -1,11 +1,17 @@
 from database import db
 from models.client import Client
+from flask import jsonify
 
 def saveDataDb(filteredData):
     #tengo que guardar los datos en la base de datos si es que no existen, si ya existen solo debo tomar sus id y ponerlo en el detalle de pedido o pedido
     #si no existe guardar cliente, sino tomar el id
     todos_clientes = Client.query.all()
-    for cliente in todos_clientes:
-        filteredData['email'] == cliente['email']
-        print(f"{filteredData['email']} es igual a:   {cliente['email']}")
-        return filteredData['email'] == cliente['email']
+    if todos_clientes:
+
+        for data in filteredData:
+            for clienteDb in todos_clientes:
+                if data['Email'] == clienteDb['email']:
+                 id_cliente = clienteDb['id_cliente']
+                 return id_cliente 
+    else:        
+        return f"no hay clientes cargados aun"

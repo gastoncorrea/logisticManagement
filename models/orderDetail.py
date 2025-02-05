52088730488c4ff40e977e88bafafff9a3cc37e6
Gaston <gastoncorrea90e@gmail.com>
2025-02-05 10:53:31 -0300

    Add product relationship in orderDetail

--- models/orderDetail.py
+++ models/orderDetail.py
@@ -1,5 +1,6 @@
 from database import db
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import  relationship
 
 class OrderDetail(db.Model):
 
@@ -9,3 +10,5 @@ class OrderDetail(db.Model):
     cantidad = db.Column(db.Integer, nullable=False)
     Producto_id_producto = db.Column(db.Integer, ForeignKey("producto.id_producto"))
     Pedido_id_pedido = db.Column(db.Integer, ForeignKey("pedido.id_pedido"))
+
+    producto = relationship("Product", back_populates="detalles")

b9faa0109f70407649e04ec51c9f481c131e1b73
unknown <gastoncorrea90e@gmail.com>
2025-03-24 00:34:28 -0300

    add orderbyid, riderByid request

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from flask_mail import Mail
 from database import db
 import os
 from dotenv import load_dotenv
-from models.__init__ import Client, Product, Location, Order, OrderDetail,Track
+from models.__init__ import Client, Shipping, Rider, Order, OrderDetail,Track
 from services.filterData import filterData
 from services.saveData import saveDataDb
 from services.sendMail import mail
+from datetime import datetime
 
 
 
@@ -45,29 +46,144 @@ def uplodad_file():
 @app.route('/')
 def recuper_pedidos():
     mis_datos = Track.query.all()
-    resultados = []
-    for registro in mis_datos:
-        if registro.estado == "En proceso":
-            resultados.append({
-                'id_pedido' : registro.pedido.id_pedido,
-                'nro_pedido' : registro.pedido.nro_pedido,
-                'fecha' : registro.pedido.fecha.strftime('%Y-%m-%d'),
-                'nombre_cliente' : registro.pedido.cliente.nombre,
-                #'email':registro.cliente.email,
-                'provincia' : registro.pedido.ubicacion.provincia,
-                'direccion': registro.pedido.ubicacion.direccion,
-                'cp': registro.pedido.ubicacion.codigo_postal,
+    
+    # Inicializamos las listas para cada estado
+    no_entregados = []
+    entregados = []
+    en_camino = []
+    en_proceso = []
+    
+    for registro in mis_datos[::-1]:
+        pedido = registro.pedido
+        estado = registro.estado.lower() 
+        
+        if estado == "no entregado":
+                no_entregados.append({
+                'id_pedido': pedido.id_pedido,
+                'nro_pedido': pedido.nro_pedido,
+                'fecha': pedido.fecha.strftime('%Y-%m-%d'),
+                'nombre_cliente': pedido.cliente.nombre,
+                'email': pedido.cliente.email,
+                'provincia': pedido.ubicacion.provincia,
+                'direccion': pedido.ubicacion.direccion,
+                'cp': pedido.ubicacion.codigo_postal,
                 'estado': registro.estado
-                }
-            )
+                })
+                mis_datos.remove(registro)
+        elif estado == "entregado":
+                entregados.append({
+                'id_pedido': pedido.id_pedido,
+                'nro_pedido': pedido.nro_pedido,
+                'fecha': pedido.fecha.strftime('%Y-%m-%d'),
+                'nombre_cliente': pedido.cliente.nombre,
+                'email': pedido.cliente.email,
+                'provincia': pedido.ubicacion.provincia,
+                'direccion': pedido.ubicacion.direccion,
+                'cp': pedido.ubicacion.codigo_postal,
+                'estado': registro.estado
+                })
+                mis_datos.remove(registro)
+        elif estado == "en camino":
+                en_camino.append({
+                'id_pedido': pedido.id_pedido,
+                'nro_pedido': pedido.nro_pedido,
+                'fecha': pedido.fecha.strftime('%Y-%m-%d'),
+                'nombre_cliente': pedido.cliente.nombre,
+                'email': pedido.cliente.email,
+                'provincia': pedido.ubicacion.provincia,
+                'direccion': pedido.ubicacion.direccion,
+                'cp': pedido.ubicacion.codigo_postal,
+                'estado': registro.estado
+                })
+                mis_datos.remove(registro)
+        elif estado == "en proceso":
+            en_proceso.append({
+                'id_pedido': pedido.id_pedido,
+                'nro_pedido': pedido.nro_pedido,
+                'fecha': pedido.fecha.strftime('%Y-%m-%d'),
+                'nombre_cliente': pedido.cliente.nombre,
+                'email': pedido.cliente.email,
+                'provincia': pedido.ubicacion.provincia,
+                'direccion': pedido.ubicacion.direccion,
+                'cp': pedido.ubicacion.codigo_postal,
+                'estado': registro.estado
+                })
+            mis_datos.remove(registro)
+        # Crear el diccionario final con las listas clasificadas
+    resultados = {
+        "no_entregados": no_entregados,
+        "entregados": entregados,
+        "en_camino": en_camino,
+        "en_proceso": en_proceso
+    }
+    print(resultados)
+    # Devolver los resultados en formato JSON
     return jsonify(resultados)
 
-@app.route('/<int:id>')
+@app.route('/detail/<int:id>')
 def recuperar_detalle_pedidos(id):
     detalle_pedido = OrderDetail.query.filter_by(Pedido_id_pedido=id).all()
     detalle = [{'id_detalle_pedido': orderDetail.id_Detalle_Pedido, 'producto': orderDetail.producto.nombre_producto, 'cantidad': orderDetail.cantidad} for orderDetail in detalle_pedido]
     return jsonify(detalle)
 
+@app.route('/order/<int:id>')
+def recuperar_un_pedido(id):
+    pedido = Order.query.filter_by(id_pedido=id).first()
+    enviar_pedido = {
+        'id_pedido': pedido.id_pedido,
+        'nro_pedido': pedido.nro_pedido
+    }
+    return jsonify(enviar_pedido)
 
+@app.route('/riders')
+def recuperar_riders():
+    find_riders = Rider.query.all()
+    riders = []
+    for rider in find_riders:
+        riders.append({
+            'id_rider': rider.id_rider,
+            'nombre': rider.nombre,
+            'apellido': rider.apellido,
+            'email': rider.email,
+            'dni': rider.dni,
+        })
+    return jsonify(riders)   
+        
+@app.route('/shipping', methods=['POST'])
+def create_shipping():
+    print(request)
+    # Obtener los datos enviados desde el frontend
+    data = request.get_json()
+
+    # Validar si los campos requeridos están presentes
+    if not data.get('fecha') or not data.get('nro_pedido') or not data.get('id_rider'):
+        return jsonify({'error': 'Campos requeridos faltantes'}), 400   
+     # Buscar el pedido correspondiente por 'nro_pedido'
+    order = Order.query.filter_by(nro_pedido=data['nro_pedido']).first()
+    if not order:
+        return jsonify({'error': 'Pedido no encontrado'}), 404 
+    rider = Rider.query.filter_by(id_rider=data['id_rider']).first()
+    if not rider:
+        return jsonify({'error': 'Rider no encontrado'}), 404
+    # Crear un nuevo envío
+    shipping = Shipping(
+        fecha=data['fecha'],
+        id_pedido=order.id_pedido,
+        id_rider=rider.id_rider,
+        desde=data.get('desde', ''),  # Puedes ajustar según sea necesario
+        hasta=data.get('hasta', 'cualquier lado')   # Puedes ajustar según sea necesario
+    )
+    track = Track(
+        fecha = datetime.now(),
+        estado = "En camino",
+        Pedido_id_pedido = order.id_pedido
+    )
+    # Agregar a la base de datos y hacer commit
+    db.session.add(shipping)
+    db.session.add(track)
+    db.session.commit()
+     # Responder con un mensaje de éxito
+    return jsonify({'message': 'Envío creado exitosamente', 'id_envio': shipping.id_envio}), 201
+    
 with app.app_context():
     db.create_all()
--- models/shipping.py
+++ models/shipping.py
@@ -8,7 +8,7 @@ class Shipping(db.Model):
     id_envio = db.Column(db.Integer, primary_key = True, autoincrement = True)
     fecha = db.Column(db.Date(), nullable = False)
     desde = db.Column(db.String(50))
-    hasta = db.Column(db.String(50), nullable = False)
+    hasta = db.Column(db.String(50))
     id_pedido = db.Column(db.Integer, ForeignKey("pedido.id_pedido"))
     id_rider = db.Column(db.Integer, ForeignKey("riders.id_rider"))
     

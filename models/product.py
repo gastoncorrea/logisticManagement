49d0a025d32962a01362f3a242e8ac67b341d27e
unknown <gastoncorrea90e@gmail.com>
2025-05-15 00:10:24 -0300

    name product field fixed

--- models/product.py
+++ models/product.py
@@ -6,6 +6,6 @@ class Product(db.Model):
     __tablename__ = 'producto'
 
     id_producto = db.Column(db.Integer, primary_key=True, autoincrement = True)
-    nombre_producto = db.Column(db.String(100), nullable=False)
+    nombre_producto = db.Column(db.String(200), nullable=False)
 
     detalles = relationship("OrderDetail", back_populates = "producto")
